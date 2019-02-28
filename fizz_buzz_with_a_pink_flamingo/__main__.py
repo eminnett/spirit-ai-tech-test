be267d3d4c80326a5dafd1ce330a7948c92012c4
Edward Minnett <ed@methodic.io>
2019-02-28 07:13:28 +0000

    Ensure fizz_buzz_with_a_pink_flamingo arguments are correctly ordered

--- fizz_buzz_with_a_pink_flamingo/__main__.py
+++ fizz_buzz_with_a_pink_flamingo/__main__.py
@@ -6,9 +6,12 @@ if 'fizz_buzz_with_a_pink_flamingo' in sys.argv:
 
 start = 0
 end = 100
+# TODO: Add error handling when receibing non integer arguments
 if len(sys.argv) > 0:
     start = int(sys.argv[0])
 if len(sys.argv) > 1:
     end = int(sys.argv[1])
+
+start, end = min(start, end), max(start, end)
 
 Parser(start, end).generate_output()
