d70b8d3486c66d8a0a693948c641c5e0f2f7dd6f
Edward Minnett <ed@methodic.io>
2019-02-28 07:07:01 +0000

    Allow fizz_buzz_with_a_pink_flamingo range to be passed as optional arguments

--- fizz_buzz_with_a_pink_flamingo/__main__.py
+++ fizz_buzz_with_a_pink_flamingo/__main__.py
@@ -1,3 +1,14 @@
+import sys
 from parser import Parser # pylint: disable=no-name-in-module
 
-Parser().generate_output()
+if 'fizz_buzz_with_a_pink_flamingo' in sys.argv:
+    sys.argv.remove('fizz_buzz_with_a_pink_flamingo')
+
+start = 0
+end = 100
+if len(sys.argv) > 0:
+    start = int(sys.argv[0])
+if len(sys.argv) > 1:
+    end = int(sys.argv[1])
+
+Parser(start, end).generate_output()
