bcfbd56e0a02c4dad70a6035456948a2a2b57c2e
Edward Minnett <ed@methodic.io>
2019-02-28 07:29:23 +0000

    Add is_fibonacci to FizzBuzz Parser

--- fizz_buzz_with_a_pink_flamingo/parser.py
+++ fizz_buzz_with_a_pink_flamingo/parser.py
@@ -1,9 +1,16 @@
+from math import sqrt
+
 class Parser:
     def __init__(self, start = 0, end = 100):
         self.range = range(start, end + 1)
 
     def is_multiple_of(self, n, x):
         return x % n == 0
+
+    def is_fibonacci(self, n):
+        lower_condition = self.is_multiple_of(1, sqrt(5*n**2 - 4))
+        upper_condition = self.is_multiple_of(1, sqrt(5*n**2 + 4))
+        return lower_condition or upper_condition
 
     def generate_output(self):
         print("This will ultimately return the parsed FizzBuzz With a Pink Flamingo output.")
--- fizz_buzz_with_a_pink_flamingo/test_parser.py
+++ fizz_buzz_with_a_pink_flamingo/test_parser.py
@@ -19,5 +19,24 @@ class ParserTest(unittest.TestCase):
         self.assertTrue(parser.is_multiple_of(3, 6))
         self.assertTrue(parser.is_multiple_of(3, 120))
 
+    def test_is_fibonacci(self):
+        parser = Parser()
+
+        self.assertTrue(parser.is_fibonacci(1))
+        self.assertTrue(parser.is_fibonacci(2))
+        self.assertTrue(parser.is_fibonacci(3))
+        self.assertTrue(parser.is_fibonacci(5))
+        self.assertTrue(parser.is_fibonacci(8))
+        self.assertTrue(parser.is_fibonacci(13))
+        self.assertTrue(parser.is_fibonacci(21))
+
+        self.assertFalse(parser.is_fibonacci(4))
+        self.assertFalse(parser.is_fibonacci(6))
+        self.assertFalse(parser.is_fibonacci(7))
+        self.assertFalse(parser.is_fibonacci(9))
+        self.assertFalse(parser.is_fibonacci(10))
+
+        
+
 if __name__ == '__main__':
     unittest.main()
