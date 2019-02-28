923307a66d1585a72145f0bc2edc7cbf106b74c5
Edward Minnett <ed@methodic.io>
2019-02-28 07:22:25 +0000

    Add is_multiple_of to FizzBuzz Parser

--- fizz_buzz_with_a_pink_flamingo/parser.py
+++ fizz_buzz_with_a_pink_flamingo/parser.py
@@ -1,6 +1,9 @@
 class Parser:
     def __init__(self, start = 0, end = 100):
         self.range = range(start, end + 1)
-    
+
+    def is_multiple_of(self, n, x):
+        return x % n == 0
+
     def generate_output(self):
         print("This will ultimately return the parsed FizzBuzz With a Pink Flamingo output.")
--- fizz_buzz_with_a_pink_flamingo/test_parser.py
+++ fizz_buzz_with_a_pink_flamingo/test_parser.py
@@ -9,6 +9,15 @@ class ParserTest(unittest.TestCase):
         parser = Parser(start, end)
 
         self.assertTrue(parser.range == range(start, end + 1))
+    
+    def test_is_multiple_of(self):
+        parser = Parser()
+
+        self.assertFalse(parser.is_multiple_of(3, 5))
+
+        self.assertTrue(parser.is_multiple_of(3, 3))
+        self.assertTrue(parser.is_multiple_of(3, 6))
+        self.assertTrue(parser.is_multiple_of(3, 120))
 
 if __name__ == '__main__':
     unittest.main()
