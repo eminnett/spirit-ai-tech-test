5f6eb1475111b9819662b48831abd4798018ca3c
Edward Minnett <ed@methodic.io>
2019-03-03 21:11:05 +0000

    Allow fizz_buzz_with_a_pink_flamingo to be executed more easily

--- fizzbuzz_with_a_pink_flamingo/__main__.py
+++ fizzbuzz_with_a_pink_flamingo/__main__.py
@@ -1,20 +1,21 @@
+"""The FizzBuzz program"""
 import sys
 from parser import Parser # pylint: disable=no-name-in-module
 
-folder_name = 'fizzbuzz_with_a_pink_flamingo'
-namespaces = ['../{}'.format(folder_name), folder_name, '.']
-for namespace in namespaces:
-    if namespace in sys.argv:
-        sys.argv.remove(namespace)
+def execute_fizz_buzz():
+    """nodoc"""
+    folder = 'fizzbuzz_with_a_pink_flamingo'
+    args = [arg for arg in sys.argv if folder not in arg and arg != '.']
 
-start = 0
-end = 100
-# TODO: Add error handling when receibing non integer arguments
-if len(sys.argv) > 0:
-    start = int(sys.argv[0])
-if len(sys.argv) > 1:
-    end = int(sys.argv[1])
+    start = 0
+    end = 100
+    if len(args) > 0:
+        start = int(args[0])
+    if len(args) > 1:
+        end = int(args[1])
 
-start, end = min(start, end), max(start, end)
+    start, end = min(start, end), max(start, end)
 
-Parser(start, end).print_output()
+    Parser(start, end).print_output()
+
+execute_fizz_buzz()
