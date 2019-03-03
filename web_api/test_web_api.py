4d93799f6a55aef9ca7702ab99919b05af8d998f
Edward Minnett <ed@methodic.io>
2019-03-03 14:05:06 +0000

    Add web api routes for the FizzBuzz program

--- web_api/test_web_api.py
+++ web_api/test_web_api.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 import web_api
 
 @pytest.fixture
@@ -7,6 +8,38 @@ def client():
     client = web_api.app.test_client()
     yield client
 
+fizzbuzz_route = '/api/v1.0/fizzbuzz-with-a-pink-flamingo'
+
 def test_root_request(client):
-    rv = client.get('/')
-    assert b'Nothing to see here!' in rv.data
+    response = client.get('/')
+    assert b'Please use the appropriate API route for your request.' in response.data
+
+
+def test_fizzbuzz_with_a_pink_flamingo_with_defaults(client):
+    data = parse_response(client.get(fizzbuzz_route))
+    assert len(data) == 101
+    assert data[:5] == ["Flamingo", "Flamingo", "Flamingo", "Flamingo", "4"]
+    assert data[-5:] == ["Fizz", "97", "98", "Fizz", "Buzz"]
+
+
+def test_fizzbuzz_with_a_pink_flamingo_with_start(client):
+    data = parse_response(client.get(fizzbuzz_route + '/90'))
+    assert data == ['FizzBuzz', '91', '92', 'Fizz', '94', 'Buzz', 'Fizz', '97', '98', 'Fizz', 'Buzz']
+
+    data = parse_response(client.get(fizzbuzz_route + '/110'))
+    assert data == ['Buzz', '101', 'Fizz', '103', '104', 'FizzBuzz', '106', '107', 'Fizz', '109', 'Buzz']
+
+
+def test_fizzbuzz_with_a_pink_flamingo_with_start_and_end(client):
+    data = parse_response(client.get(fizzbuzz_route + '/5/10'))
+    assert data == ['Flamingo', 'Fizz', '7', 'Flamingo', 'Fizz', 'Buzz']
+
+    data = parse_response(client.get(fizzbuzz_route + '/5/0'))
+    assert data == ['Flamingo', 'Flamingo', 'Flamingo', 'Flamingo', '4', 'Flamingo']
+
+    data = parse_response(client.get(fizzbuzz_route + '/6760/6770'))
+    assert data == ['Buzz', '6761', 'Fizz', '6763', '6764', 'Pink Flamingo', '6766', '6767', 'Fizz', '6769', 'Buzz']
+
+
+def parse_response(response):
+    return json.loads(response.get_data(as_text=True))
--- web_api/web_api.py
+++ web_api/web_api.py
@@ -1,11 +1,25 @@
 #!flask/bin/python
-from flask import Flask
+import subprocess
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Nothing to see here!"
+    return "Please use the appropriate API route for your request."
+
+fizzbuzz_route = '/api/v1.0/fizzbuzz-with-a-pink-flamingo'
+@app.route(fizzbuzz_route, methods=['GET'], defaults={'start': 0, 'end': 100})
+@app.route(fizzbuzz_route + '/<int:start>', methods=['GET'], defaults={'end': 100})
+@app.route(fizzbuzz_route + '/<int:start>/<int:end>', methods=['GET'])
+def fizzbuzz_with_a_pink_flamingo(start, end):
+    process_params = ['python', '../fizzbuzz_with_a_pink_flamingo', str(start), str(end)]
+    process = subprocess.Popen(process_params, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) 
+    out, err = process.communicate()
+    results = []
+    if err == None:
+        results = str(out).replace("b'", '').replace("\\n'", '').split(', ')
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
